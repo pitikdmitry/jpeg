0649027bb75fb9e700e562b3ce03a8fdb2d807cc
pitikdmitry <pitik.dmitry@mail.ru>
2018-05-08 22:30:41 +0300

    add searching comment

--- decoder/app.py
+++ decoder/app.py
@@ -4,14 +4,19 @@ from decoder.bytes_array import BytesArray
 from decoder.exceptions.exceptions import BadMarkerException
 
 
-def parse_ffd8(img):
-    if img[0:4] != b'ffd8':
+def parse_ffd8(bytes_array: BytesArray):
+    if bytes_array[0] + bytes_array[1] != b'ffd8':
         raise BadMarkerException
-    print(img[0:4])
 
 
-def parse_fffe(img):
-    pass
+def parse_fffe(bytes_array: BytesArray):
+    if bytes_array[2] + bytes_array[3] != b'fffe':
+        raise BadMarkerException
+    #   find ff db to cut comment
+    ff_db_index = bytes_array.find_pair_index("ff", "db")
+    if ff_db_index != -1:
+        comment = bytes_array[4:ff_db_index]
+        return
 
 
 with open("favicon.jpg", "rb") as f:
@@ -19,6 +24,7 @@ with open("favicon.jpg", "rb") as f:
     bytes_array = BytesArray(img)
 
     try:
-        parse_ffd8(img)
+        parse_ffd8(bytes_array)
+        parse_fffe(bytes_array)
     except BadMarkerException as e:
         print("Bad marker exc")
--- decoder/bytes_array.py
+++ decoder/bytes_array.py
@@ -9,3 +9,20 @@ class BytesArray(list):
         for i in range(0, len(img_hex), n):
             byte_hex = img_hex[i:i + n]
             super(BytesArray, self).append(byte_hex)
+
+    def find_pair_index(self, str1: str, str2: str) -> int:
+        str1 = bytes(str1, encoding="UTF-8")
+        str2 = bytes(str2, encoding="UTF-8")
+
+        for i in range(0, len(self)):
+            if i + 1 < len(self):
+                first_str = self[i]
+                second_str = self[i + 1]
+                s1 = first_str + second_str
+                s2 = str1 + str2
+                if first_str + second_str == str1 + str2:
+                    return i
+            else:
+                return -1
+
+        return -1
