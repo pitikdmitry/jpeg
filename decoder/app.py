a238d4aa7c8642eaf89bc52c680b2c57c22b6d77
pitikdmitry <pitik.dmitry@mail.ru>
2018-05-10 11:04:39 +0300

    dc koeff read done

--- decoder/app.py
+++ decoder/app.py
@@ -1,8 +1,9 @@
 from decoder.bytes_array import BytesArray
 from decoder.exceptions.exceptions import BadMarkerException
 from decoder.image_info import ImageInfo
+from decoder.utils.array_utils import create_zeros_list
 from decoder.utils.haffman_tree import HaffmanTree
-from decoder.utils.zig_zag import zig_zag_order
+from decoder.utils.zig_zag import ZigZag
 
 
 def parse_ffd8(bytes_array: BytesArray, image_info: ImageInfo):    #  заголовок
@@ -27,6 +28,7 @@ def parse_ffdb(bytes_array: BytesArray, image_info: ImageInfo):     #   табл
 
     ff_c0_index = bytes_array.find_pair("ff", "c0")     #   ffc0 является правой границей для крайней таблицы квантования
     ff_db_indexes.append(ff_c0_index)
+    zig_zag = ZigZag()
 
     for i in range(0, len(ff_db_indexes) - 1):    #   -1 для того чтобы +1 взять как верхнюю границу таблицы квантования т.е. ffc0
         header_index = ff_db_indexes[i] + 2
@@ -34,7 +36,7 @@ def parse_ffdb(bytes_array: BytesArray, image_info: ImageInfo):     #   табл
         end_index = ff_db_indexes[i + 1]
         header = bytes_array[header_index:header_index + 3]  # длина заголовока всегда 3 байта
         quantization_arr = bytes_array[start_index:end_index]
-        quantization_table = zig_zag_order(quantization_arr)
+        quantization_table = zig_zag.zig_zag_order(quantization_arr)
         image_info.quantization_tables.append(quantization_table)
 
 
@@ -81,17 +83,19 @@ def parse_ffda(bytes_array: BytesArray, image_info: ImageInfo): # start of scan
         coded_data_binary += str(ch_2)
 
     #   в цикле для количества компонентов считываем по 2 байта(информацию о них)
+    zig_zag = ZigZag()
     if amount_of_components == 3:
         y_info = ffda_data[5:7]
-        parse_y_channel(coded_data_binary, y_info, image_info=image_info)
+        parse_y_channel(coded_data_binary, y_info, image_info, zig_zag)
         cb_info = ffda_data[7:9]
         cr_info = ffda_data[9:11]
     elif amount_of_components == 1:
         y_info = ffda_data[5:7]
-        parse_y_channel(coded_data_binary, y_info, image_info=image_info)
+        parse_y_channel(coded_data_binary, y_info, image_info, zig_zag)
 
 
-def parse_y_channel(code: str, y_info: str, image_info: ImageInfo):
+def parse_y_channel(code: str, y_info: str, image_info: ImageInfo, zig_zag: ZigZag):
+    result_array = create_zeros_list(8, 8)
     haffman_trees = image_info.haffman_trees
     dc_haff_tree = None
     ac_haff_tree = None
@@ -105,11 +109,16 @@ def parse_y_channel(code: str, y_info: str, image_info: ImageInfo):
             ac_haff_tree = tree
 
     arr_for_index = [0]
+    #   reading dc
     dc = dc_haff_tree.get_next_value(code, arr_for_index)
     dc_koef = 0
     if dc.value != 0:
         dc_koef = dc_haff_tree.get_next_n_bits(code, arr_for_index, dc.value)
-
+        if dc_koef[0] != "1":
+            dc_koef = int(dc_koef, 2) - 2 ** dc.value + 1
+        dc_koef = int(dc_koef, 2)
+    zig_zag.put_in_zig_zag(result_array, dc_koef)
+    #   reading ac
     pass
 
 
--- decoder/utils/zig_zag.py
+++ decoder/utils/zig_zag.py
@@ -1,46 +1,99 @@
 from decoder.utils.array_utils import create_zeros_array, create_zeros_list
 
 
-def zig_zag_order(arr):
-    N, M = 8, 8
-    i, j, counter = 0, 0, 0
-    result = create_zeros_list(N, M)
-    result[i][j] = arr[counter]
-    counter += 1
+class ZigZag:
+    def __init__(self):
+        self._up = True
+        self._i = 0
+        self._j = 0
+        self._counter = 0
 
-    up = True
-    while counter < N * M - 1:
-        if not up:  #   down
-            while 0 <= i < N - 1 and 0 < j < M:
-                j -= 1
-                i += 1
-                result[i][j] = arr[counter]
-                counter += 1
-            if 0 < i < N - 1:
-                i += 1
-                result[i][j] = arr[counter]
-                counter += 1
-            elif 0 <= j < M - 1:
-                j += 1
-                result[i][j] = arr[counter]
-                counter += 1
-            up = True
-        else:   #   up
-            while 0 < i < N and 0 <= j < M - 1:
-                j += 1
-                i -= 1
-                result[i][j] = arr[counter]
-                counter += 1
-            if 0 <= j < M - 1:
-                j += 1
-                result[i][j] = arr[counter]
-                counter += 1
-            elif 0 <= i < N - 1:
-                i += 1
-                result[i][j] = arr[counter]
-                counter += 1
-            up = False
+    def zig_zag_order(self, arr):
+        N, M = 8, 8
+        i, j, counter = 0, 0, 0
+        result = create_zeros_list(N, M)
+        result[i][j] = arr[counter]
+        counter += 1
 
-    return result
+        up = True
+        while counter < N * M - 1:
+            if not up:  #   down
+                while 0 <= i < N - 1 and 0 < j < M:
+                    j -= 1
+                    i += 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                if 0 < i < N - 1:
+                    i += 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                elif 0 <= j < M - 1:
+                    j += 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                up = True
+            else:   #   up
+                while 0 < i < N and 0 <= j < M - 1:
+                    j += 1
+                    i -= 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                if 0 <= j < M - 1:
+                    j += 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                elif 0 <= i < N - 1:
+                    i += 1
+                    result[i][j] = arr[counter]
+                    counter += 1
+                up = False
 
+        return result
 
+    def put_in_zig_zag(self, result, element):
+        N, M = 8, 8
+
+        if self._i == 0 and self._j == 0:
+            result[self._i][self._j] = element
+            self._counter += 1
+            return
+
+        while self._counter < N * M - 1:
+            if not self._up:  #   down
+                while 0 <= self._i < N - 1 and 0 < self._j < M:
+                    self._j -= 1
+                    self._i += 1
+                    result[self._i][self._j] = element
+                    self._counter += 1
+                    return
+                if 0 < self._i < N - 1:
+                    self._i += 1
+                    result[self._i][self._j] = element
+                    self._counter += 1
+                    return
+                elif 0 <= self._j< M - 1:
+                    self._j += 1
+                    result[i][j] = element
+                    self._counter += 1
+                    return
+                self._up = True
+            else:   #   up
+                while 0 < self._i < N and 0 <= self._j < M - 1:
+                    self._j += 1
+                    self._i -= 1
+                    result[self._i][self._j] = element
+                    self._counter += 1
+                    return
+                if 0 <= self._j< M - 1:
+                    self._j += 1
+                    result[self._i][self._j] = element
+                    self._counter += 1
+                    return
+                elif 0 <= self._i < N - 1:
+                    self._i += 1
+                    result[self._i][self._j] = element
+                    self._counter += 1
+                    return
+                self._up = False
+
+        return
